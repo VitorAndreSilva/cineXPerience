72c830a236a885a1d02ad9f5a9cd937b52feb238
VitorAndreSilva <alunovitorandre@gmail.com>
2025-11-04 09:11:42 -0300

    FEAT: Adiciona componente de rodapé

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import FooterComponent from './components/FooterComponent.vue';
 </script>
 
 <template>
   <RouterView />
+  <FooterComponent />
 </template>
 
 <style scoped>
+
 </style>
