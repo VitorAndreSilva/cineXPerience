6bff2186f94f36cd4947adf48ca468e6308faf48
rafael-otto <rafael.august.otto.ifc@gmail.com>
2025-11-06 08:52:20 -0300

    FEAT: Finalizando o header #3

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import HeaderComponent from './components/HeaderComponent.vue';
 </script>
 
 <template>
-  <RouterView />
+  <div class="bg-[#050A1C] text-white font-sans h-screen w-screen">
+    <HeaderComponent />
+    <RouterView />
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { url } from 'node:inspector'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '&': fileURLToPath(new URL('./public', import.meta.url)),
     },
   },
 })
