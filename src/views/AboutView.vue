5368cba98f144bd2373f94b8cf4539223286b307
VitorAndreSilva <alunovitorandre@gmail.com>
2025-11-17 11:21:04 -0300

    FEAT: Melhora a estrutura do AboutView com estilo e conteúdo descritivo

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,4 +1,9 @@
 <script setup></script>
 <template>
-    <h1>Sobre o CineXPerience</h1>
+    <section class="text-center py-6">
+        <div>
+            <h1 class="text-5xl font-bold">Sobre o CineXPerience</h1>
+            <h3 class="text-2xl py-4 text-[#94A3B8]">Uma viagem nostálgica ao cinema dos anos 90 e 2000</h3>
+        </div>
+    </section>
 </template>
