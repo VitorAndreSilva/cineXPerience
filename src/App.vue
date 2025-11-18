dcd7f0c78eeb078688380b8db58aec0165218027
rafael-otto <rafael.august.otto.ifc@gmail.com>
2025-11-18 17:07:44 -0300

    Revert "Merge pull request #23 from VitorAndreSilva/FEAT-22"
    This reverts commit f574f462d07b1e5dfc2a850dab741e5feb339ff4, reversing
    changes made to 36f56f567d3ab54760ebda12c5ba64d4d97006b6.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <script setup>
 import { RouterView } from 'vue-router'
-import HeaderComponent from './components/HeaderComponent.vue';
-import FooterComponent from './components/FooterComponent.vue';
 </script>
 
 <template>
-  <div class="bg-[#050A1C] text-white font-sans flex flex-col min-h-screen justify-between">
-    <HeaderComponent />
-    <RouterView />
-    <FooterComponent class="bg-[#050A1C]" />
-  </div>
+  <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
-import AboutView from '@/views/AboutView.vue'
-import MoviesDetailView from '@/views/MoviesDetailView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: AboutView,
-    },
-    {
-      path: '/detail',
-      name: 'detail',
-      component: MoviesDetailView,
-    },
+    {}
   ],
 })
 
